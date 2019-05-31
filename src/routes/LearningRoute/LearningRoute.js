b8fd74029b8a6a14ab3a55d509a616b93ee8f81f
JizongL <2015rpro@gmail.com>
2019-05-31 11:39:44 -0400

    past day 3.2 feedback test

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -5,18 +5,22 @@ class LearningRoute extends Component {
     totalScore:null,
     nextWord:'',
     correctCount:0,
-    incorrectCount:0
+    incorrectCount:0,
+    isCorrect:null,
+    answer:'',
+    attempted:'',
+    currentword:''
 
   }
   componentDidMount(){
     LanguageApiService.getLearnWord()
-    .then(data=>{
-      console.log(data.totalScore,'test total')
+    .then(data=>{      
       this.setState({
         totalScore:data.totalScore,
         nextWord:data.nextWord,
         correctCount:data.wordCorrectCount,
-        incorrectCount:data.wordIncorrectCount})      
+        incorrectCount:data.wordIncorrectCount,
+        currentword:data.nextWord})      
       })
     }
   handleSubmitGuessWord=(e)=>{
@@ -27,19 +31,27 @@ class LearningRoute extends Component {
       guess:guess_word.value
     }
     LanguageApiService.submitGuessWord(guessWord)
-    .then(data=>{
-      
-      this.setState({correctCount:data.wordCorrectCount})
+    .then(data=>{      
+      this.setState({
+      totalScore:data.totalScore,
+      nextWord:data.nextWord,  
+      correctCount:data.wordCorrectCount,    
+      incorrectCount:data.wordIncorrectCount,
+      isCorrect:data.isCorrect,
+      answer:data.answer,
+      attempted:guess_word.value})
+      alert(this.state.isCorrect?'yes':'no')
     })
   }
-  render() {
-    console.log(this.state.correctCount)
+  render() {   
+    console.log(this.state,'test state')
+    const isCorrectMessage=this.state.isCorrect?'You were correct! :D':'Good try, but not quite right :('
     return (
       <section>
         <div>
           current score:{this.state.totalScore}
         </div>
-        <h2>Translate the word:</h2>
+        <h3>Translate the word:</h3>
         <span>{this.state.nextWord}</span>
         <p>Your total score is: {this.state.totalScore}</p>
         <form className='guess_word_form'
@@ -57,14 +69,24 @@ class LearningRoute extends Component {
           
           ></input>
           <button type='submit' className='guess_word_submit'>
-          Submit your answer
+          Try another word!
           </button>
         </form>
         <div>
-        You have answered this word correctly {this.state.correctCount} times.
+          You have answered this word correctly {this.state.correctCount} times.
         </div>
         <div>
-        You have answered this word incorrectly {this.state.incorrectCount} times.
+         You have answered this word incorrectly {this.state.incorrectCount} times.
+        </div>
+        <h2>{isCorrectMessage}</h2>
+        <div className='DisplayScore'>
+        <p>Your total score is: {this.state.totalScore}</p>
+        </div>
+
+        <div className='DisplayFeedback'>
+          <p>
+          The correct translation for {this.state.currentword} was {this.state.answer} and you chose {this.state.attempted}! 
+          </p>
         </div>
       </section>
     );
